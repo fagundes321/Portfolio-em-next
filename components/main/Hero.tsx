775723aba8176100beb8c44fdda55231d1cf9888
fagundes321 <victorfagundes123@gmail.com>
2025-03-09 19:42:38 -0300

    Atualização na pagina inicial

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import HeroContent from '../sub/HeroContent'
 
 const Hero = () => {
   return (
@@ -10,6 +11,7 @@ const Hero = () => {
             >
                 <source src='/blackhole.webm' type='video/webm'/>
         </video>
+        <HeroContent/>
     </div>
   )
 }
