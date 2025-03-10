0f309d746ff9c9221f749953cba96123dcb455c6
fagundes321 <victorfagundes123@gmail.com>
2025-03-10 17:01:13 -0300

    Foi feita alterações no Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#1f242d] ">
-        <Navbar/>
+      <Navbar />
       <div className="container mx-auto px-12 py-4">
-        <HeroSection/>
+        <HeroSection />
       </div>
     </main>
   );
