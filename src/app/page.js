91da264509bc2d2d7198fdf882caf6a5ed4d114d
fagundes321 <victorfagundes123@gmail.com>
2025-03-18 21:26:21 -0300

    Foi adicionado a section de contato

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,13 @@
-import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import ClickSpark from "./components/ClickSpark/ClickSpark";
-import Threads from "./components/Backgrounds/Threads";
-import Particles from "./components/Backgrounds/Particles"
+import EmailSection from "./components/EmailSection"
 
 export default function Home() {
   return (
-    <main className="select-none">
+    <main className="select-none flex flex-col">
       <ClickSpark
         sparkColor="#fff"
         sparkSize={10}
@@ -18,57 +16,16 @@ export default function Home() {
         duration={400}
       >
         <Navbar />
-        {/* <div className="absolute top-0 left-0 w-full h-full z-[-1]">
-        <Particles
-          particleColors={["#ffffff", "#ffffff"]}
-          particleCount={900}
-          particleSpread={10}
-          speed={0.1}
-          particleBaseSize={100}
-          moveParticlesOnHover={true}
-          alphaParticles={false}
-          disableRotation={false}
-        />
-      </div> */}
+
         {/* Seção Hero */}
-        <div className="flex flex-col select-none">
-          <div className="container mt-15 mx-auto px-12 py-4">
-            <HeroSection />
-          </div>
-        </div>
-
-        {/* Threads entre as seções Hero e About */}
-        <div style={{ width: "100%", height: "600px" }}>
-          <Threads
-            amplitude={1}
-            distance={0}
-            enableMouseInteraction={true}
-            color={[124 / 255, 240 / 255, 61 / 255]} // Cor em RGB normalizada
-          />
-        </div>
-
-        {/* Seção About */}
-        <div className="flex flex-col select-none">
-          <div className="container mt-15 mx-auto px-12 py-4">
-            <AboutSection />
-          </div>
-        </div>
-
-        {/* Threads entre as seções About e Projects */}
-        <div style={{ width: "100%", height: "600px" }}>
-          <Threads
-            amplitude={1}
-            distance={0}
-            enableMouseInteraction={true}
-            color={[124 / 255, 240 / 255, 61 / 255]} // Cor em RGB normalizada
-          />
-        </div>
-
-        {/* Seção Projects */}
-        <div className="flex flex-col select-none">
-          <div className="container mt-15 mx-auto px-12 py-4">
-            <ProjectsSection />
-          </div>
+        <div className="container mt-15 mx-auto px-12 py-4">
+          <HeroSection />
+          <br></br>
+          {/* Seção About */}
+          <AboutSection />
+          {/* Seção Projects */}
+          <ProjectsSection />
+        <EmailSection/>
         </div>
       </ClickSpark>
     </main>
