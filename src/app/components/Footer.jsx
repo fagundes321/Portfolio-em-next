b3eb2d7d86503a89a1d47b03c8045d0fbdf54ba3
fagundes321 <victorfagundes123@gmail.com>
2025-03-25 23:39:36 -0300

    alterações no footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import AOS from "aos";
-import "aos/dist/aos.css"; 
+import "aos/dist/aos.css";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="footer border-t z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white" >
-      <div className="container p-8 flex flex-col sm:flex-row justify-between items-center">
-        <img
+    <footer className="footer border-t z-10 border-t-[#33353F] text-white w-full flex justify-center">
+      <div className="w-full max-w-3xl p-8 flex flex-col items-center justify-center text-center">
+    
+         <Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
+          <img
           src="images/logo.png"
           className="h-[50px] w-auto sm:h-[40px] md:h-[50px]"
           alt="Logo"
         />
-        <p className="mt-4 sm:mt-0 text-center sm:text-left text-slate-500">
-          Todos os Direitos Reservados
+        </Link>
+        <p className="mt-4 text-slate-500">
+          © {new Date().getFullYear()} Todos os direitos reservados.
         </p>
       </div>
     </footer>
